fbb09b4cdd139f83dac31250646ebb8a7924623a
Mike Evans <mike.evans88@gmail.com>
2016-05-12 19:33:02 -0400

    first commit

---
+++ PHP_Project/index.php
@@ -0,0 +1,13 @@
+<?php
+$message = "Hello World";
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><?php echo $message ?></title>
+  </head>
+  <body>
+    <?php echo $message; ?>
+  </body>
+</html>
